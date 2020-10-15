d15f1857ef76faaf06075e89485408323543c63d
walysonfelipe <walysonfelipe25@gmail.com>
2020-10-15 11:34:15 -0300

    commit

---
+++ 12-finalizando-front-end-mobile-do-app/src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,258 @@
+import React, { useCallback, useEffect, useState, useMemo } from 'react';
+import { useRoute, useNavigation } from '@react-navigation/native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import Icon from 'react-native-vector-icons/Feather';
+import { Platform, Alert } from 'react-native';
+import { format } from 'date-fns';
+import {
+  Container,
+  Header,
+  BackButton,
+  HeaderTitle,
+  UserAvatar,
+  Content,
+  ProvidersListContainer,
+  ProvidersList,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderName,
+  Calendar,
+  Title,
+  OpenDatePickerButton,
+  OpenDatePickerText,
+  Schedule,
+  Section,
+  SectionTitle,
+  SectionContent,
+  Hour,
+  HourText,
+  CreateAppointmentButton,
+  CreateAppointmentButtonText,
+} from './styles';
+import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+import { Provider } from '../Dashboard';
+
+interface RouteParams {
+  providerId: string;
+}
+
+interface AvailabilityItem {
+  hour: number;
+  available: boolean;
+}
+
+const CreateAppointment: React.FC = () => {
+  const route = useRoute();
+  const { user } = useAuth();
+  const navigation = useNavigation();
+
+  const routeParams = route.params as RouteParams;
+
+  const [availability, setAvailability] = useState<AvailabilityItem[]>([]);
+  const [showDatePicker, setShowDateTimePicker] = useState(false);
+  const [selectedHour, setSelectedHour] = useState(0);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(
+    routeParams.providerId,
+  );
+
+  useEffect(() => {
+    api.get('providers').then((response) => setProviders(response.data));
+  }, []);
+
+  const navigateBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const handleSelectProvider = useCallback((providerId: string) => {
+    setSelectedProvider(providerId);
+  }, []);
+
+  const handleToggleDatePicker = useCallback(() => {
+    setShowDateTimePicker((state) => !state);
+  }, []);
+
+  const handleDateChanged = useCallback(
+    (event: any, date: Date | undefined) => {
+      if (Platform.OS === 'android') {
+        setShowDateTimePicker(false);
+      }
+
+      if (date) {
+        setSelectedDate(date);
+      }
+    },
+    [],
+  );
+
+  const morningAvailability = useMemo(() => {
+    return availability
+      .filter(({ hour }) => hour < 12)
+      .map(({ hour, available }) => {
+        return {
+          hour,
+          available,
+          hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+        };
+      });
+  }, [availability]);
+
+  const afternoonAvailability = useMemo(() => {
+    return availability
+      .filter(({ hour }) => hour >= 12)
+      .map(({ hour, available }) => {
+        return {
+          hour,
+          available,
+          hourFormatted: format(new Date().setHours(hour), 'HH:00'),
+        };
+      });
+  }, [availability]);
+
+  useEffect(() => {
+    api
+      .get(`providers/${selectedProvider}/day-availability`, {
+        params: {
+          day: selectedDate.getDate(),
+          month: selectedDate.getMonth() + 1,
+          year: selectedDate.getFullYear(),
+        },
+      })
+      .then((response) => {
+        setAvailability(response.data);
+        setSelectedHour(0);
+      });
+  }, [selectedDate, selectedProvider]);
+
+  const handleSelectHour = useCallback((hour: number) => {
+    setSelectedHour(hour);
+  }, []);
+
+  const handleCreateAppointment = useCallback(async () => {
+    try {
+      const date = new Date(selectedDate);
+
+      date.setHours(selectedHour);
+      date.setMinutes(0);
+
+      await api.post('appointments', {
+        provider_id: selectedProvider,
+        date,
+      });
+
+      navigation.navigate('AppointmentCreated', { date: date.getTime() });
+    } catch (error) {
+      Alert.alert(
+        'Erro ao criar agendamente',
+        'Ocorreu um erro ao tentar criar o agendamento, tente novamente.',
+      );
+    }
+  }, [navigation, selectedDate, selectedHour, selectedProvider]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={navigateBack}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+          <HeaderTitle>Cabeleireiros</HeaderTitle>
+        </BackButton>
+
+        <UserAvatar source={{ uri: user.avatar_url }} />
+      </Header>
+
+      <Content>
+        <ProvidersListContainer>
+          <ProvidersList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={providers}
+            keyExtractor={(provider) => provider.id}
+            renderItem={({ item: provider }) => (
+              <ProviderContainer
+                selected={provider.id === selectedProvider}
+                onPress={() => handleSelectProvider(provider.id)}
+              >
+                <ProviderAvatar source={{ uri: provider.avatar_url }} />
+                <ProviderName selected={provider.id === selectedProvider}>
+                  {provider.name}
+                </ProviderName>
+              </ProviderContainer>
+            )}
+          />
+        </ProvidersListContainer>
+
+        <Calendar>
+          <Title>Escolha a data</Title>
+
+          <OpenDatePickerButton onPress={handleToggleDatePicker}>
+            <OpenDatePickerText>Selecionar outra data</OpenDatePickerText>
+          </OpenDatePickerButton>
+
+          {showDatePicker && (
+            <DateTimePicker
+              mode="date"
+              onChange={handleDateChanged}
+              display="calendar"
+              textColor="#f4ede8"
+              value={selectedDate}
+            />
+          )}
+        </Calendar>
+
+        <Schedule>
+          <Title>Escolha o horário</Title>
+
+          <Section>
+            <SectionTitle>Manhã</SectionTitle>
+
+            <SectionContent>
+              {morningAvailability.map(({ hourFormatted, hour, available }) => (
+                <Hour
+                  enabled={available}
+                  available={available}
+                  key={hourFormatted}
+                  selected={selectedHour === hour}
+                  onPress={() => handleSelectHour(hour)}
+                >
+                  <HourText selected={selectedHour === hour}>
+                    {hourFormatted}
+                  </HourText>
+                </Hour>
+              ))}
+            </SectionContent>
+          </Section>
+
+          <Section>
+            <SectionTitle>Tarde</SectionTitle>
+
+            <SectionContent>
+              {afternoonAvailability.map(
+                ({ hourFormatted, hour, available }) => (
+                  <Hour
+                    enabled={available}
+                    selected={selectedHour === hour}
+                    available={available}
+                    key={hourFormatted}
+                    onPress={() => handleSelectHour(hour)}
+                  >
+                    <HourText selected={selectedHour === hour}>
+                      {hourFormatted}
+                    </HourText>
+                  </Hour>
+                ),
+              )}
+            </SectionContent>
+          </Section>
+        </Schedule>
+
+        <CreateAppointmentButton onPress={handleCreateAppointment}>
+          <CreateAppointmentButtonText>Agendar</CreateAppointmentButtonText>
+        </CreateAppointmentButton>
+      </Content>
+    </Container>
+  );
+};
+
+export default CreateAppointment;
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# -aulas-bootcamp-GoStack13-rocketseat-
+![Logo Rocketseat](https://camo.githubusercontent.com/d25397e9df01fe7882dcc1cbc96bdf052ffd7d0c/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f676f6c64656e2d77696e642f626f6f7463616d702d676f737461636b2f6865616465722d6465736166696f732e706e67)
+
+<center>
+# Repositório de Aulas Bootcamp GoStack 11 - RocketSeat
+Esse repositório contém as aulas e códigos desenvolvidos por mim (Thiago Cedran Santos) durante as aulas do curso GoStack 11 da Rocketseat
+</center>
+
+### Índice de Aulas
+
+Abaixo você encontra o índice e conteúdo de cada aula:
+
+| Índice | README |
+| ------ | ------ |
+| 00 | [Back-end com Node.js](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/00-backend-com-nodejs) |
+| 01 | [Front-end com ReactJS](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/01-frontend-com-reactjs) |
+| 02 | [Mobile com React Native](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/02-mobile-com-react-native) |
+| 03 | [Primeiro projeto com Node.JS](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/03-primeiro-projeto-node) |
+| 04 | [Iniciando back-end do app](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/04-iniciando-back-end) |
+| 05 | [Primeiro projeto com React](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/05-primeiro-projeto-react) |
+| 06 | [Iniciando front-end web](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/06-gobarber-web) |
+| 07 | [Iniciando aplicativo mobile](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/07-appgobarber) |
+| 08 | [Iniciando aplicativo mobile](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/08-arquitetura-e-testes-no-nodejs) |
+| 09 | [Continuando back-end do app](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/09-continuando-back-end-do-app) |
+| 10 | [Finalizando back-end do app](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/10-finalizando-back-end-do-app) |
+| 11 | [Finalizando front-end web do app](https://github.com/thiagocdn/aulas-bootcamp-GoStack11-rocketseat/tree/master/11-finalizando-front-end-web-do-app) |
+
+Espero que o repositório ajude quem precisar!
+Fiquem a vontade para entrar em contato se tiver alguma dúvida ou sugestão.
